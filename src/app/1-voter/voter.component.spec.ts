38a0295e084e4cca0d7ef04626075c14b3170687
OsamaBACS <osama.solution@gmail.com>
2021-08-03 08:59:36 +0300

    initialCommit

---
+++ src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing'
+import { By } from '@angular/platform-browser';
+import { VoterComponent } from './voter.component';
+
+describe('VoterComponent', () => {
+
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+
+    // fixture.nativeElement;  return an HTML element
+    // fixture.debugElement; it is a wrapper around component
+
+  });
+
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+
+    // root DOM element
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain("21");
+  });
+});
